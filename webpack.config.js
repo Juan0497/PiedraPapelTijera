a10c4a16b2620fcc3337a9072c344c4f259ae950
unknown <juan97f@gmail.com>
2021-08-02 17:48:28 -0600

    creacion de webpack y configuracion de github pages

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path")
+
+const basePath = __dirname;
+const distPath = "docs";
+
+const webpackInitConfig = {
+    mode: "development",
+    resolve: {
+        extensions: [".js"]
+    },
+    entry: {
+        app: ["./index.html"],
+    },
+    output: {
+        path: path.join(basePath, distPath),
+        filename: "[name].bundle.js"
+    },
+    module: {
+        rules: [
+             {
+              test: /\.html$/,
+              use: [
+              {
+                  loader: 'html-loader',
+               },
+                 ],
+            },
+         ]
+    }
+};
+module.exports = webpackInitConfig;
