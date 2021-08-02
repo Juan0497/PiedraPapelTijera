63242e47c6c31b0a54d54b5f82feddf588446c17
unknown <juan97f@gmail.com>
2021-08-02 01:35:00 -0600

    proyecto completo

---
+++ main.js
@@ -0,0 +1,43 @@
+
+// let js moderno
+//var viejo 
+//cost constante 
+//prompt es como un alert donde usted mete info
+// tolowercase funcion que pone todo en minuscula 
+// console.error para que imprima en consola como un
+// console.warning para que se vea amarillo en consola 
+// || es un or
+
+let tijera = "tijera";
+let piedra = "piedra";
+let papel = "papel";
+
+let obtener = function( ) {
+
+    let accion = document.getElementById("accion").value;
+    let accion2 = document.getElementById("accion2").value;
+
+    if (accion === accion2) {
+        console.log("Empate");
+    
+    }
+    if (accion === papel && accion2 === piedra) {
+        console.log("Gana usuario 1");
+    }
+    if (accion == papel && accion2 == tijera) {
+        console.log("Gana el usuario 2");
+    }
+    if (accion === tijera && accion2 === piedra) {
+        console.log("Gana usuario 2");
+    }
+    if (accion === tijera && accion2 === papel) {
+        console.log("Gana el usuario 1");
+    }
+    if (accion == piedra && accion2 == papel) {
+        console.log("Gana el usuario 2");
+    }
+    if (accion == piedra && accion2 == tijera) {
+        console.log("Gana el usuario 1");
+    }
+    
+}
